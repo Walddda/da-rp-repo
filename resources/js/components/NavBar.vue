2fbcc82135c2178b3532f0f2ff65a4a43742ddc4
Samuel <samuel.g.anmeldung@gmail.com>
2021-11-02 16:27:56 +0100

    axios/nav-bar
    Co-authored-by: gabjetski <gabjetski@users.noreply.github.com>

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -27,6 +27,10 @@
             <UploadIcon class="h-6 w-6" aria-hidden="true" />
           </button>
 
+            <div>
+              <label class="text-gray-400"> {{ $page.props.auth.user.username }} </label>
+            </div>
+
           <!-- Profile dropdown -->
           <Menu as="div" class="ml-3 relative">
             <div>
@@ -36,17 +40,26 @@
 
               </MenuButton>
             </div>
-            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+            <transition enter-active-class="transition ease-out duration-500" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
               <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <MenuItem v-slot="{ active }">
+                <!--<MenuItem v-slot="{ active }">
                   <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Profile</a>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
                   <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
-                  <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
-                </MenuItem>
+                  <a :href="route('logout')" method="post" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Logout</a>
+                </MenuItem>-->
+                <BreezeDropdownLink>
+                  My Profile
+                </BreezeDropdownLink>
+                <BreezeDropdownLink>
+                  Settings
+                </BreezeDropdownLink>
+                <BreezeDropdownLink :href="route('logout')" method="post" as="button">
+                  Log Out
+                </BreezeDropdownLink>
               </MenuItems>
             </transition>
           </Menu>
@@ -71,6 +84,7 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 
 
 
+import BreezeDropdownLink from '@/Components/DropdownLink.vue'
 
 import { UploadIcon, MenuIcon, XIcon } from '@heroicons/vue/outline';
 
@@ -97,6 +111,7 @@ export default {
         UploadIcon,
         MenuIcon,
         XIcon,
+        BreezeDropdownLink,
     },
 
     setup(){
